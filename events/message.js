20c6c4fa68c91ec7cbe720386c7f00e33dbc0a52
jajoscript <javieralmarzabucarey@gmail.com>
2019-12-05 16:32:49 -0600

    cambio de planes

--- events/message.js
+++ events/message.js
@@ -1,14 +1,14 @@
 module.exports = (client, message) => {
+    // Testing
+    console.log(`[${message.author.username}]:\"${message.content}\"`);
+    
     // Control de errores.
-    // if(!message.content.startsWith(process.env.PREFIX || '#')) return;
+    if(!message.content.startsWith(process.env.PREFIX)) return;
     if(message.author.bot) return;
 
     //Definicion de comandos y argumentos.
     const arguments = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
     const command = arguments.shift().toLowerCase();
-
-    // TEsting
-    console.log(`[${message.author.username}]:\"${message.content}\"`);
 
     let Command = client.commands.get(command);
     if(!Command) return;
