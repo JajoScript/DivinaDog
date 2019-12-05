66c520e1f11d2ebb3a4eb26bebc36c69a250fd8b
jajoscript <javieralmarzabucarey@gmail.com>
2019-12-05 13:30:02 -0600

    Controladores

--- main.js
+++ main.js
@@ -2,3 +2,34 @@
 require('dotenv').config();
 
 // Importación de modulos.
+const Discord = require('discord.js');
+let { readdirSync } = require('fs');
+
+// Instanciación de la clase Discord.
+const client = new Discord.Client();
+
+// Instanciación de los comandos.
+client.commands = new Discord.Collection();
+
+// Controlador de comandos.
+for(const file of readdirSync('./Commands/')){
+
+    // Filtro de archivos.
+    if(file.endsWith('.js')){
+        let fileName = file.substring(0, file.length - 3);
+        let fileContent = require(`./Commands/${file}`);
+        client.commands.set(fileName, fileContent);
+    }
+}
+
+// Controlador de Eventos.
+for(const file of readdirSync('./Events/')){
+    // Filtro de archivos.
+    if(file.endsWith('.js')){
+        let fileName = file.substring(0, file.length - 3);
+        let fileContent = require(`/Events/${file}`);
+        client.on(fileName, fileContent.bind(null, client));
+
+        delete require.cache[require.resolve(`./Events/${file}`)];
+    }
+}
