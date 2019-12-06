28e05b363517c0890f439d738f6584d8eda3030a
jajoscript <javieralmarzabucarey@gmail.com>
2019-12-05 20:21:56 -0600

    mejoras al comando say y agregue el comando clear

---
+++ commands/clear.js
@@ -0,0 +1,30 @@
+// Dependencies.
+const Discord = require('discord.js');
+
+module.exports = (client, message, arguments) => {
+    console.log(arguments);
+    console.log(arguments[0]);
+    
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Disculpe, pero usted no tiene los poderes para hacer eso.");
+    if(!arguments[0]) return message.reply("Disculpe, necesito un numero para trabajar.");
+
+    if(arguments[0]>100){
+        message.channel.send("Disculpe, pero solo puedo limpiar 100 mensajes a la vez")
+        .then((msg) => {
+            msg.delete(5000);
+        })
+        .cath((error) => {
+            console.error('[Error]', error);
+        });
+    }
+    else{
+        message.channel.bulkDelete(arguments[0])
+            .then(()=>{
+                message.channel.send(`Limpiado ${arguments[0]} mensajes`).then(msg => msg.delete(5000));
+            })
+            .catch((error) => {
+                console.error('[Error]', error);
+            });  
+    }
+
+}
--- commands/say.js
+++ commands/say.js
@@ -1,5 +1,6 @@
 module.exports = (client, message, arguments) => {
     let text = arguments.join(' ');
     if(!text) return message.channel.send('Debes escribir un mensaje');
+    message.delete().catch();
     message.channel.send(text);
 }
