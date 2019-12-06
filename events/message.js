636433d8c68d0deac09380ca778b67f97218efa7
jajoscript <javieralmarzabucarey@gmail.com>
2019-12-05 18:00:52 -0600

    mensajes aprefix

--- events/message.js
+++ events/message.js
@@ -2,6 +2,10 @@ module.exports = (client, message) => {
     // Testing
     console.log(`[${message.author.username}]:\"${message.content}\"`);
     
+    // Saludos
+    if(message.content.toLowerCase() == 'gd') return message.reply('buenos dias!');
+    else if(message.content.toLowerCase() == 'gn') return message.reply('buenas noches!');
+    
     // Control de errores.
     if(!message.content.startsWith(process.env.PREFIX)) return;
     if(message.author.bot) return;
