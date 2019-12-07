3cfd9ae54c7a02e2cd1f690e53ee62c23a60a276
jajoscript <javieralmarzabucarey@gmail.com>
2019-12-06 21:20:53 -0600

    comando destroy activado

--- commands/destroy.js
+++ commands/destroy.js
@@ -1,6 +1,25 @@
 // Importación de modulos.
 
 // Creación del comando.
-module.export = (client, message, arguments) => {
+module.exports = (client, message, arguments) => {
+    const developer = 'JAJO';
+    console.log(message.author.username);
 
+    if(message.author.username === 'JAJO'){
+        message.reply('Usted es el desarrollador, puede usar este comando');
+        
+        message.channel.send("Auto destruccion activada.")
+        .then(() => {
+            client.destroy().then(() => {
+                process.exit();
+            });
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+    else {
+        message.reply('Usted no es el desarrollador, no puede usar este comando');
+    }
+    
 };
