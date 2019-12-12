ee892a00803d18407c7dac4af5f1d097d84baef1
jajoscript <javieralmarzabucarey@gmail.com>
2019-12-11 19:39:19 -0600

    r6 comand

---
+++ commands/r6.js
@@ -0,0 +1,18 @@
+// Importación de modulos.
+const R6API = require('r6api.js');
+const r6api = new R6API(process.env.R6_EMAIL, process.env.R6_PASSWORD);
+
+// Creación del comando.
+module.exports = async (client, message, arguments) => {
+    // Username input
+    console.log(arguments);
+    const username = arguments[0];
+    const platform = arguments[1];
+    
+    if(!username || !platform){
+        message.reply('Disculpe, debe escribir un usurio y la plataforma');
+        return
+    }
+    
+    
+};
