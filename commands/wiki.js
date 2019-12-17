f2667ff35165fac5e7e7ca64bceb3aeffe5e8b7c
jajoscript <javieralmarzabucarey@gmail.com>
2019-12-17 00:21:13 -0600

    descontinuacion del comando wiki

--- commands/wiki.js
+++
@@ -1,9 +0,0 @@
-// Importacion de dependencias.
-var wiki = require('wikijs').default;
-
-// Creación del comando.
-module.exports = async(client, message, arguments) => {
-    const query = arguments.join(' ');
-
-   wiki().random()
-}
