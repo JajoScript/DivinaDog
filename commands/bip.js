b9edb4c48d9e4945370eb37f64addb0cb192d9cc
jajoscript <javieralmarzabucarey@gmail.com>
2019-12-17 06:43:55 -0600

    almacenando datos

--- commands/bip.js
+++ commands/bip.js
@@ -2,10 +2,19 @@
 const bip = require('bip');
 const {RichEmbed} = require('discord.js');
 const { format } = require('timeago.js');
+const fs = require('fs');
+
+// Importación de datos.
+let bipCards = JSON.parse(fs.readFileSync("./data/bip.json", "utf8"));
 
 // Creación del comando
 module.exports = async (client, message, arguments) => {
 
+    // Catch User
+    let bipUser = message.author.username
+    console.log(`[BIP_USER] ${bipUser}`);
+
+    // Bip Query
     const cardNumber = arguments.join(' ');
     if(!cardNumber || cardNumber.length < 8){
         message.reply("Disculpe, necesito el numero de la bip para trabajar.");
@@ -13,21 +22,24 @@ module.exports = async (client, message, arguments) => {
     }
     else if(cardNumber) {
         await bip(cardNumber)
-        .then(value => {
-            console.log(value);
-            const bipEmbed = new RichEmbed()
-                .setColor("BLUE")
-                .setTitle(`Tarjeta: ${value.number}`)
-                .addField('Estado: ', value.message)
-                .addField('Saldo: ', value.balance)
-                .addField('Ultimo uso: ', format(value.date))
+            .then(value => {
+                console.log(value);
+                const bipEmbed = new RichEmbed()
+                    .setColor("BLUE")
+                    .setTitle(`Tarjeta: ${value.number}`)
+                    .addField('Estado: ', value.message)
+                    .addField('Saldo: ', value.balance)
+                    .addField('Ultimo uso: ', format(value.date))
 
-            message.reply(bipEmbed);
-        })
-        .catch(error => {
-            console.error(error);
-            message.reply('Disculpe' + error.message);
-        });
+                message.reply(bipEmbed);
+            })
+            .catch(error => {
+                console.error(error);
+                message.reply('Disculpe' + error.message);
+            });
+
+        // Save Bip
+       
     }
     
 
---
+++ commands/mybip.js
@@ -0,0 +1,7 @@
+// Importación de dependencias.
+const fs = require('fs');
+
+// Creacion del comando.
+module.exports = (client, message, arguments) => {
+    
+};
