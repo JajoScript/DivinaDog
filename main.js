db4313314a380cb533eb4d8ed37f03be2eb252d1
jajoscript <javieralmarzabucarey@gmail.com>
2019-12-17 22:18:26 -0600

    solucion problemas main

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // Importación de variables de entorno.
-require('dotenv').config();
+require("dotenv").config();
 
 // Importación de modulos.
-const Discord = require('discord.js');
-let { readdirSync } = require('fs');
+const Discord = require("discord.js");
+let { readdirSync } = require("fs");
 
 // Instanciación de la clase Discord.
 const client = new Discord.Client();
@@ -12,7 +12,7 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
 // Controlador de comandos.
-for(const file of readdirSync('./commands/')){
+for(const file of readdirSync("./commands/")){
 
     // Filtro de archivos.
     if(file.endsWith('.js')){
@@ -23,9 +23,9 @@ for(const file of readdirSync('./commands/')){
 }
 
 // Controlador de Eventos.
-for(const file of readdirSync('./events/')){
+for(const file of readdirSync("./events/")){
     // Filtro de archivos.
-    if(file.endsWith('.js')){
+    if(file.endsWith(".js")){
         let fileName = file.substring(0, file.length - 3);
         let fileContent = require(`./events/${file}`);
         client.on(fileName, fileContent.bind(null, client));
@@ -37,8 +37,8 @@ for(const file of readdirSync('./events/')){
 // Inicio de sesión
 client.login(process.env.TOKEN)
     .then(()=>{
-        console.log(`Estoy listo, soy ${client.user.tag}`);
+        // console.log(`Estoy listo, soy ${client.user.tag}`);
     })
     .catch((error)=>{
-        console.error('[ERROR] Error al iniciar sesión', error);
+        // console.error('[ERROR] Error al iniciar sesión', error);
     })
