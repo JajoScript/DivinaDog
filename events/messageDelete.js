ea0b3bdc6c8bf92dc41695091669e2c279715f1f
jajoscript <javieralmarzabucarey@gmail.com>
2019-12-17 21:42:42 -0600

    solucion problemas evento messageDelete

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,5 +1,5 @@
 // Creacion del evento.
 module.exports = (client, message) => {
     client.channels.get(process.env.CHANNEL_ID);
-    console.log(`[Mensaje Eliminado]:[${message.author.username}]`);
-}
+    // console.log(`[Mensaje Eliminado]:[${message.author.username}]`);
+};
