111399368e7b897cbed62ca3320dd99455cea050
jajoscript <javieralmarzabucarey@gmail.com>
2019-12-21 17:25:24 -0600

    funcionalidad lista

--- commands/fish.js
+++ commands/fish.js
@@ -8,36 +8,71 @@ const Fish = require("../models/fishs");
 module.exports = async (client, message, arguments1 ) => {
     let rollfish = Math.floor(Math.random() * 7) +1;
     
-    var fishGiven;
+    var fishGiven, golden, normal, cart;
 
     if(rollfish === 1){
         message.channel.send("Felicitaciones, " + message.author.username + "! pescaste: :tropical_fish: +3 puntos");
-        fishGiven = 3;
+        golden = 1; normal = 0; cart = 0;
 
     }else if(rollfish === 2){
         message.channel.send("Felicitaciones, " + message.author.username + "! pescaste: :fish: +2 puntos");
-        fishGiven = 2;
+        golden = 0; normal = 1; cart = 0;
 
     }else { 
         message.channel.send("Que mal :(, " + message.author.username + "! pescaste: :shopping_cart: +0 puntos");
-        fishGiven = 1;
+        golden = 0; normal = 0; cart = 1;
     }
+
+    console.log("[Golden]", golden);
+    console.log("[normal]", normal);
+    console.log("[cart]", cart);
+
 
     await Fish.findOne({
         userID : message.author.id
     }, (error, schema) => {
         if(error) console.log(error);
 
-        console.log(schema);
-
         if(!schema){
             console.log("[DB] Squema no encontrado");
 
+            new_Schema = new Fish({
+                _id : mongoose.Types.ObjectId(),
+                username: message.author.username,
+                userID : message.author.id,
+                golden: golden,
+                normal: normal,
+                cart: cart,
+            });
+        
+
+            console.log("[DB] creado nuevo usuario fish");
+
+            new_Schema.save()
+                .then(resultado => console.log(resultado))
+                .catch(error => console.log(error))
 
         }
         else if(schema){
             console.log("[DB] Squema encontrado");
             
+            const newGolden = schema.golden + golden;
+            const newNormal = schema.normal + normal;
+            const newCart = schema.cart + cart;
+
+            console.log("[newGolden]", newGolden);
+            console.log("[newNormal]", newNormal);
+            console.log("[newCart]", newCart);
+
+            schema.updateOne({
+                golden: newGolden,
+                normal: newNormal,
+                cart: newCart
+            }).catch(error => console.log(error));
+
+            schema.save()
+                .then((resultado) => console.log(resultado))
+                .catch((error) => console.log(error));
         }
     });
 };
