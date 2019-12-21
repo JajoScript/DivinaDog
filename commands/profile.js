c626bd5195baf71d97ad78e708fd534bdce21424
jajoscript <javieralmarzabucarey@gmail.com>
2019-12-21 00:51:21 -0600

    command profile v1

--- commands/profile.js
+++ commands/profile.js
@@ -1,6 +1,64 @@
 // Importacion de dependencias.
+const { RichEmbed } = require("discord.js");
+const mongoose = require("mongoose");
+const { format } = require('timeago.js');
+
+// Importación del modelo.
+const Bip = require("../models/bips.js");
+const Respect = require("../models/respects.js");
 
 // Creacion del comando.
-module.exports = (client, message, arguments) => {
+module.exports = async (client, message, arguments) => {
+
+    const profileEmbed = new RichEmbed()
+        .setColor("RANDOM")
+        .setTitle(":bust_in_silhouette: Perfil de " + message.author.username)
+        .setDescription("Información del usuario:")
+        .addField(":tada: Fecha de creacion:", format(message.author.createdAt))
+        .addField(":ticket: Identificador:", message.author.id)
+        .setFooter("Server: " + message.guild)
+        .setThumbnail(message.author.displayAvatarURL)
+
+    await Bip.findOne({
+        userID: message.author.id
+    }, (error, schema) => {
+        if(error){
+            console.log(`[ERROR] ${error}`);
+        }
+        
+        console.log(schema);
+        
+        // Asignación de datos segun esquema.
+        if(!schema){
+            console.log("[DB] esquema no encontrado");
+            profileEmbed.addField(":credit_card: Tarjetas: ", "Tarjetas no encontradas :(");
+
+        }
+        else if(schema){
+            console.log("[DB] esquema encontrado");
+            profileEmbed.addField(":credit_card: Tarjetas: ", schema.bip)
+        }
+    });
+
+    await Respect.findOne({
+        userID: message.author.id
+    }, (error, schema) => {
+        if(error){
+            console.log(`[ERROR] ${error}`);
+        }
+        
+        console.log(schema);
+        // Asignación de datos segun esquema.
+        if(!schema){
+            console.log("[DB] esquema no encontrado");
+            profileEmbed.addField(":trophy: Respetos: ", "Aún no tienes respetos :(");
+
+        }
+        else if(schema){
+            console.log("[DB] esquema encontrado");
+            profileEmbed.addField(":trophy: Respetos: ", schema.respects)
+        }
+    });
     
+    message.channel.send(profileEmbed);
 };
