5d53ec42b473d88acf949edae46b21c0f478ca4b
Javier Almarza <javieralmarzabucarey@gmail.com>
2021-04-22 10:37:04 -0400

    solucionado

--- events/message.js
+++ events/message.js
@@ -1,6 +1,10 @@
 module.exports = (client, message) => {
+    //Definicion de comandos y argumentos.
+    const arguments = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
+    const command = arguments.shift().toLowerCase();
+
+
     // Control de errores.
-    
     if(!message.guild) return console.log(`[DM][@ ${message.author.username}]:\"${message.content}\"`);
 
     // Testing
@@ -11,7 +15,7 @@ module.exports = (client, message) => {
     else if(message.content.toLowerCase() == 'gn') return message.reply('Buenas noches!');
     else if(message.content.toLowerCase() == 'te amo') return message.reply('Yo te amo más!');
     else if (message.content == "https://giant.gfycat.com/LeafyRashAmethystgemclam.mp4" || message.content == "https://gfycat.com/joyousrealgoldfish" ){
-        message.delete()
+        message.channel.bulkDelete(1)
         message.reply("Que estai mandando maldito desgraciado")
     }
 
@@ -24,9 +28,7 @@ module.exports = (client, message) => {
     if(!message.content.startsWith(process.env.PREFIX)) return;
     if(message.author.bot) return;
 
-    //Definicion de comandos y argumentos.
-    const arguments = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
-    const command = arguments.shift().toLowerCase();
+    
 
     let Command = client.commands.get(command);
     if(!Command) return;
